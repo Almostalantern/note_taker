87402e210b0ee6397fb9753f537120ece836a148
John Ciancone <jackccinc@gmail.com>
2021-02-01 17:45:04 -0500

    now with working delete

--- files/routes/api.js
+++ files/routes/api.js
@@ -2,21 +2,22 @@ const router = require("express").Router();
 const notes = require("../db/Notes");
 
 
-router.get("./notes", function (req, res){
+
+router.get("/notes", function (req, res){
     notes.getNotes().then(notes => {
         return res.json(notes)
     }).catch(err => res.status(500).json(err))
 })
 
-router.post("./notes", function (req, res){
+router.post("/notes", function (req, res){
     notes.writeNote(req.body).then(notes => {
         return res.json(notes)
     }).catch(err => res.status(500).json(err))
 })
 
-router.get("/:id", function (req, res){
-    notes.getNotes(req.params.id).then(notes => {
-        return res.json(notes)
+router.delete("/notes/:id", function (req, res){
+    notes.deleteNote(req.params.id).then(notes => {
+        return res.json(notes);
     }).catch(err => res.status(500).json(err))
 })
 
--- files/routes/view.js
+++ files/routes/view.js
@@ -5,8 +5,8 @@ router.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
-router.get("./notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "notes.html"));
+router.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
 module.exports= router;
